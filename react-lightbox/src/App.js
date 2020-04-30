43da2bc1019cc854a7cf0f39d7da50914e53fc67
Erin Dawson <erinedawson@gmail.com>
2020-04-30 17:47:07 -0600

    add images to lightbox section

--- react-lightbox/src/App.js
+++ react-lightbox/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-    <section class="lightbox">
-    
+    <section className="lightbox">
+    <img src="/images/artem-sapegin-8c6eS43iq1o-unsplash.jpg" alt="Sunset Behind Mountains by Artem Sapegin via Unsplash"/>
+    <img src="/images/ashley-knedler-OwpgxILRR7c-unsplash.jpg" alt="Antelope Canyon by Ashley Knedler via Unsplash"/>
+    <img src="/images/cristina-gottardi-R4y_E5ZQDPg-unsplash.jpg" alt="Mountains And Trees Reflecting On Lake by Cristina Gottardi via Unsplash"/>
     </section>
 
     </div>
