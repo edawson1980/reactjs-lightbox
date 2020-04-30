f01fc6dcb5eaa4a493e6be5ecb139699c3b73263
Erin Dawson <erinedawson@gmail.com>
2020-04-30 17:57:05 -0600

    add onClick event handler to images

--- react-lightbox/src/App.js
+++ react-lightbox/src/App.js
@@ -13,9 +13,9 @@ function App() {
     <div className="App">
 
     <section className="lightbox">
-      <img src="/images/artem-sapegin-8c6eS43iq1o-unsplash.jpg" alt="Sunset Behind Mountains by Artem Sapegin via Unsplash"/>
-      <img src="/images/ashley-knedler-OwpgxILRR7c-unsplash.jpg" alt="Antelope Canyon by Ashley Knedler via Unsplash"/>
-      <img src="/images/cristina-gottardi-R4y_E5ZQDPg-unsplash.jpg" alt="Mountains And Trees Reflecting On Lake by Cristina Gottardi via Unsplash"/>
+      <img onClick={openModal} src="/images/artem-sapegin-8c6eS43iq1o-unsplash.jpg" alt="Sunset Behind Mountains by Artem Sapegin via Unsplash"/>
+      <img onClick={openModal} src="/images/ashley-knedler-OwpgxILRR7c-unsplash.jpg" alt="Antelope Canyon by Ashley Knedler via Unsplash"/>
+      <img onClick={openModal} src="/images/cristina-gottardi-R4y_E5ZQDPg-unsplash.jpg" alt="Mountains And Trees Reflecting On Lake by Cristina Gottardi via Unsplash"/>
     </section>
 
     </div>
