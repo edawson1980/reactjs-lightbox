f618ea848925ce22239b151b32bd4dce3dc8377d
Erin Dawson <erinedawson@gmail.com>
2020-04-30 17:51:37 -0600

    import useState

--- react-lightbox/src/App.js
+++ react-lightbox/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState }from 'react';
 import './App.css';
 
 
 function App() {
+
   return (
     <div className="App">
 
     <section className="lightbox">
-    <img src="/images/artem-sapegin-8c6eS43iq1o-unsplash.jpg" alt="Sunset Behind Mountains by Artem Sapegin via Unsplash"/>
-    <img src="/images/ashley-knedler-OwpgxILRR7c-unsplash.jpg" alt="Antelope Canyon by Ashley Knedler via Unsplash"/>
-    <img src="/images/cristina-gottardi-R4y_E5ZQDPg-unsplash.jpg" alt="Mountains And Trees Reflecting On Lake by Cristina Gottardi via Unsplash"/>
+      <img src="/images/artem-sapegin-8c6eS43iq1o-unsplash.jpg" alt="Sunset Behind Mountains by Artem Sapegin via Unsplash"/>
+      <img src="/images/ashley-knedler-OwpgxILRR7c-unsplash.jpg" alt="Antelope Canyon by Ashley Knedler via Unsplash"/>
+      <img src="/images/cristina-gottardi-R4y_E5ZQDPg-unsplash.jpg" alt="Mountains And Trees Reflecting On Lake by Cristina Gottardi via Unsplash"/>
     </section>
 
     </div>
