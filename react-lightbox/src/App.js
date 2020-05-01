8759e09a90b2e0354fd84252008672d5b16bb8fb
Erin Dawson <erinedawson@gmail.com>
2020-04-30 18:21:43 -0600

    remember how to use comments in jsx, add image visibility to overlay div

--- react-lightbox/src/App.js
+++ react-lightbox/src/App.js
@@ -22,12 +22,31 @@ function App() {
     <div className="App">
 
     <section className="lightbox">
-      <img onClick={openModal} src="/images/artem-sapegin-8c6eS43iq1o-unsplash.jpg" alt="Sunset Behind Mountains by Artem Sapegin via Unsplash"/>
-      <img onClick={openModal} src="/images/ashley-knedler-OwpgxILRR7c-unsplash.jpg" alt="Antelope Canyon by Ashley Knedler via Unsplash"/>
-      <img onClick={openModal} src="/images/cristina-gottardi-R4y_E5ZQDPg-unsplash.jpg" alt="Mountains And Trees Reflecting On Lake by Cristina Gottardi via Unsplash"/>
+      <img
+      onClick={openModal}
+      src="/images/artem-sapegin-8c6eS43iq1o-unsplash.jpg"
+      alt="Sunset Behind Mountains by Artem Sapegin via Unsplash"
+      />
+      <img
+      onClick={openModal}
+      src="/images/ashley-knedler-OwpgxILRR7c-unsplash.jpg"
+      alt="Antelope Canyon by Ashley Knedler via Unsplash"
+      />
+      <img
+      onClick={openModal}
+      src="/images/cristina-gottardi-R4y_E5ZQDPg-unsplash.jpg"
+      alt="Mountains And Trees Reflecting On Lake by Cristina Gottardi via Unsplash"
+      />
     </section>
 
-    {image.src !== `` && <div id="overlay"></div>}
+    {/*this conditional says that IF image.src isn't empty AND a div named "overlay" exists, show that overlay*/}
+    {image.src !== `` && <div id="overlay">
+      <figure>
+        <img src={image.src} alt={image.alt}/>
+        <figcaption>{image.alt}</figcaption>
+      </figure>
+    </div>}
+
     </div>
   );
 }
